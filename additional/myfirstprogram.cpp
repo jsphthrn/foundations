54a7a59392a4b8b2b27f7223f48e637fda666f79
José Yáñez <jlyp_sn@protonmail.com>
2024-06-09 15:32:32 -0600

    Include tutorial of C++ language

---
+++ additional/myfirstprogram.cpp
@@ -0,0 +1,12 @@
+#include <iostream> // header library, to use cout object (line 5)
+using namespace std; // to use standard names for object and variables from library
+
+int main () {   // declaration of a function
+    cout << "Hello World"; // cout is an object to print text (as output in terminal). << is insertion operator.
+    return 0; // By now this is just for ending main function but can be used for other real cool stuff.
+} 
+
+/* To omitt the using namespace std; line 2
+std::cout << "Hello world"
+
+*/
